c286a4c2e548bc733da853547cf62d5f0bb732c9
GEC\wuttinan.cha <wuttinan.cha@gec.co.th>
2017-06-12 18:06:48 +0700

    first commit

---
+++ resources/lang/th/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'login_header_panel' => 'Login',
+    'email' => 'E-mail',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'login_button' => 'Login',
+    'forgot_password' => 'Forgot Your Password?',
+    'login_nav' => 'Login',
+    'register_nav' => 'Register',
+];
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{ trans('lang.login_header_panel') }}</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">{{ trans('lang.email') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password" class="col-md-4 control-label">{{ trans('lang.password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+                            </div>
+                        </div>
+                        <div  class="form-group has-error animated shake">
+                            <div class="col-md-8 col-md-offset-4">
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ trans('lang.remember_me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ trans('lang.login_button') }}
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ trans('lang.forgot_password') }}
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
